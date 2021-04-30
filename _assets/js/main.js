936d26bd7fa2d0cca53fc7be7d10489c55d66803
Brian Hurst <brian.hurst@gsa.gov>
2021-04-30 14:15:08 -0400

    Toggle accordion when selected from side nav

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -1,7 +1,18 @@
 //= require gumshoe.min.js
 //= require js/uswds.min.js
 
-var spy = new Gumshoe('#toc a', {
-	nested: true,
-	nestedClass: 'active-parent'
+const spy = new Gumshoe("#toc a", {
+  nested: true,
+  nestedClass: "active-parent"
+});
+
+document.getElementById("toc").addEventListener("click", event => {
+  const hash = event.target.hash.replace("#", "");
+  const id = document.getElementById(hash);
+  if (!id || !id.classList.contains("usa-accordion__heading")) return;
+  const button = id.querySelector("button");
+
+  if (button && button.getAttribute("aria-expanded") !== "true") {
+    button.click();
+  }
 });
