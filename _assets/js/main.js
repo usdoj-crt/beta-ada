7ad662dd6a49864cedf19055196badfa20b57b4e
Brian Hurst <brian.hurst@gsa.gov>
2021-04-30 14:46:14 -0400

    Refactor to also open when navigating directly to URL

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -1,18 +1,2 @@
-//= require gumshoe.min.js
 //= require js/uswds.min.js
-
-const spy = new Gumshoe("#toc a", {
-  nested: true,
-  nestedClass: "active-parent"
-});
-
-document.getElementById("toc").addEventListener("click", event => {
-  const hash = event.target.hash.replace("#", "");
-  const id = document.getElementById(hash);
-  if (!id || !id.classList.contains("usa-accordion__heading")) return;
-  const button = id.querySelector("button");
-
-  if (button && button.getAttribute("aria-expanded") !== "true") {
-    button.click();
-  }
-});
+//= require ./toc.js
