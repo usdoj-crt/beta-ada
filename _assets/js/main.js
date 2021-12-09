dfff86e5d88820ba17acd99af9c1e0555cecfafd
JackRyan1989 <jackryan@navapbc.com>
2021-12-09 13:47:16 -0500

    Added polyfill for orientation page number.

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -1,8 +1,7 @@
 //= require js/uswds.min.js
 //= require gumshoe.polyfills.min.js
 //= require anchor.min.js
-//= require ./utils/polyfills/urlSearchParams.js
-//= require ./utils/polyfills/url-polyfill.min.js
+//= require_directory ./utils/polyfills/
 //= require ./modal.js
 //= require ./redirect-modal.js
 //= require ./pagination.js
