e545e6c61226a0169430d5f480e17103efe4e088
JackRyan1989 <jackryan@navapbc.com>
2022-03-02 12:37:12 -0500

    do pagination-build file instead of dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,8 @@ module.exports = {
   mode: process.env.NODE_ENV || "development",
   entry: './_assets/js/pagination.js',
   output: {
-    path: path.resolve(__dirname, './_assets/js/', 'dist'),
-    filename: 'pagination.js',
+    path: path.resolve(__dirname, './_assets/js/'),
+    filename: 'pagination-build.js',
   },
   module: {
     rules: [
