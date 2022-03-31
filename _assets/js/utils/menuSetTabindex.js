50e93620efff4e60f86e2272104f652809534bd0
JackRyan1989 <jackryan@navapbc.com>
2022-03-31 15:03:50 -0400

    Added a small toggle script for adding and removing tabindex

---
+++ _assets/js/utils/menuSetTabindex.js
@@ -0,0 +1,48 @@
+// Extending the functionality of the navigation.js code from USWDS
+const NON_NAV_ELEMENTS = 'body > *:not(.usa-header):not([aria-hidden])';
+const NON_NAV_HIDDEN = '[data-nav-hidden]';
+
+const MENUBUTTON = document.querySelector('button.usa-menu-btn');
+const CLOSEBUTTON = document.querySelector('button.usa-nav__close');
+
+const hideNonNavItems = () => {
+  nonNavElements = document.querySelectorAll(NON_NAV_ELEMENTS);
+
+  nonNavElements.forEach((nonNavElement) => {
+    nonNavElement.setAttribute('tabindex', '-1');
+  });
+};
+
+const showNonNavItems = () => {
+  nonNavElements = document.querySelectorAll(NON_NAV_HIDDEN);
+
+  if (!nonNavElements) {
+    return;
+  }
+
+  // Remove aria-hidden from non-header elements
+  nonNavElements.forEach((nonNavElement) => {
+    nonNavElement.removeAttribute('tabindex');
+  });
+};
+
+// Toggle all non-header elements #3527.
+const toggleNonNavItems = (active) => {
+  if (active) {
+    showNonNavItems();
+  } else {
+    hideNonNavItems();
+  }
+};
+
+// Add event listener to menu button:
+MENUBUTTON.addEventListener('click', (e) => {
+  e.preventDefault();
+  toggleNonNavItems(false);
+});
+
+// Add an event listener to the close button:
+CLOSEBUTTON.addEventListener('click', (e) => {
+  e.preventDefault();
+  toggleNonNavItems(true);
+});
