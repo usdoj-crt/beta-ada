d47724ad13d2d1509c17170de6b417eb1bdcc144
Jack Ryan <jackryan@navapbc.com>
2022-04-22 15:58:12 -0400

    Updated readme to explain initialization steps adding in snapshots increased timeout duration and set the number of retries to 1 for dev tests.

--- playwright.config.js
+++ playwright.config.js
@@ -15,18 +15,18 @@ const { devices } = require('@playwright/test');
 const config = {
   testDir: './tests',
   /* Maximum time one test can run for. */
-  timeout: 30 * 1000,
+  timeout: 60 * 1000,
   expect: {
     /**
      * Maximum time expect() should wait for the condition to be met.
      * For example in `await expect(locator).toHaveText();`
      */
-    timeout: 5000
+    timeout: 10000
   },
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  /* Retry twice on CI and once on dev */
+  retries: process.env.CI ? 2 : 1,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
