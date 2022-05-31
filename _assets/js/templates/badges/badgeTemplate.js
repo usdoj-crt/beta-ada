17eb8f72587b30069968149e4bff11595a8af375
Jack Ryan <jackryan@navapbc.com>
2022-05-31 11:49:15 -0400

    Finished button styling

--- _assets/js/templates/badges/badgeTemplate.js
+++ _assets/js/templates/badges/badgeTemplate.js
@@ -1,10 +1,10 @@
 export default function badge(title) {
     title = title.split('-').join(" ");
     return `
-    <span class="usa-button usa-button--outline">
+    <span class="usa-button usa-button--outline button-wrapper">
      ${title[0].toUpperCase() + title.substring(1)}
     <button class="usa-button usa-button--unstyled text-no-underline"
-    ><span class="text-ink text-bold text-no-underline">X</span>
+    ><span class="text-ink text-bold text-no-underline margin-x-2">X</span>
     <span class="usa-sr-only">Press enter to remove ${title} filter.</span>
     </button>
     </span>
