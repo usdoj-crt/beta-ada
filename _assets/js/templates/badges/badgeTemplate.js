0179cdd6ebefabb9209f84244904b8382f53d242
Jack Ryan <jackryan@navapbc.com>
2022-06-07 17:15:59 -0400

    Addressing chinelo's concerns

--- _assets/js/templates/badges/badgeTemplate.js
+++ _assets/js/templates/badges/badgeTemplate.js
@@ -1,10 +1,13 @@
 export default function badge(title) {
     const normalizedTitle = title.split('-').join(" ");
     return `
-    <span class="usa-button usa-button--outline button-wrapper">
+    <span class="usa-button text-normal button-wrapper">
      ${normalizedTitle[0].toUpperCase() + normalizedTitle.substring(1)}
     <button class="usa-button usa-button--unstyled text-no-underline"
-    ><span class="text-ink text-bold text-no-underline margin-left-1">X</span>
+    >
+    <span class="margin-left-05 flex-align-center"><svg fill='#162e51' aria-hidden="true" role="img">
+    <use xlink:href="/assets/img/sprite.svg#close"></use>
+        </svg></span>
     <span class="usa-sr-only">Press enter to remove ${normalizedTitle} filter.</span>
     </button>
     </span>
