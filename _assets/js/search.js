073ff5bb3f05319d343573fb82cb06651bad7eea
JackRyan1989 <jackryan@navapbc.com>
2022-07-19 11:28:07 -0400

    Removing extraneous console log used to trigger Federalist rebuild.

--- _assets/js/search.js
+++ _assets/js/search.js
@@ -4,7 +4,6 @@ if (queryString) {
   var input = document.getElementById("query");
   var params = new URLSearchParams(queryString.substring(1));
   var value = params.get("query");
-  console.log(value);
 
   if (value) input.value = value;
 }
