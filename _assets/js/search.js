274db6cd0558d10dfaff971cd1fad979b63298b4
JackRyan1989 <jackryan@navapbc.com>
2022-07-19 11:16:12 -0400

    Retriggering federalist build. If successful fire another PR to undo this change.

--- _assets/js/search.js
+++ _assets/js/search.js
@@ -4,6 +4,7 @@ if (queryString) {
   var input = document.getElementById("query");
   var params = new URLSearchParams(queryString.substring(1));
   var value = params.get("query");
+  console.log(value);
 
   if (value) input.value = value;
 }
