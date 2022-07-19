8a76c281a4e56b4bcdcd023ec24b12d6bc2e5977
JackRyan1989 <jackryan@navapbc.com>
2022-07-19 10:29:58 -0400

    Removing change that retriggered Federalist build

--- _assets/js/search.js
+++ _assets/js/search.js
@@ -5,7 +5,6 @@ const search = () => {
     const input = document.getElementById('query');
     const params = new URLSearchParams(queryString.substring(1));
     const value = params.get('query');
-    console.log(value);
 
     if (value) input.value = value;
   }
