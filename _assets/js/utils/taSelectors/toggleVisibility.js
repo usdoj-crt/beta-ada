0ab7b5e21fcc16e21d737fc859337a6fad13ccbc
JackRyan1989 <jackryan@navapbc.com>
2022-07-27 13:28:36 -0400

    Fixing number of items found counter.

--- _assets/js/utils/taSelectors/toggleVisibility.js
+++ _assets/js/utils/taSelectors/toggleVisibility.js
@@ -1,10 +1,8 @@
 export default function toggleVisibility(state) {
   const elements = Array.from(document.querySelectorAll('li.visibilityTarget'));
-  let numberVisible = elements.length;
   elements.forEach((element) => {
     if (state.length === 0) {
       element.style.display='block';
-      return numberVisible;
     }
     else {
       element.style.display='block';
@@ -12,10 +10,8 @@ export default function toggleVisibility(state) {
       state.forEach(tag => {
         if (!tagList.includes(tag)) {
           element.style.display = 'none';
-          numberVisible -= 1;
         }
       })
     }
   });
-  return numberVisible;
 }
