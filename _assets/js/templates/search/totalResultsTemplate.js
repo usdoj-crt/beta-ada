b00fa51c152ee708922761c3020c2b7d16605d38
JackRyan1989 <jackryan@navapbc.com>
2022-08-10 08:52:53 -0400

    removed extraneous role = status

--- _assets/js/templates/search/totalResultsTemplate.js
+++ _assets/js/templates/search/totalResultsTemplate.js
@@ -1,16 +1,16 @@
 export default function totalResults(resultsTotal=0, type="results") {
-    const moreThanOne = `<div markdown="0" role="status"><p class="total-results margin-y-0">${resultsTotal} ${type}s found</p></div>`;
-    const equalToOne = `<div markdown="0" role="status"><p class="total-results margin-y-0">${resultsTotal} ${type} found</p></div>`;
-    const lessThanOne = `<div markdown="0" role="status"><p class="total-results margin-y-0">No ${type}s found</p></div>`;
+    const moreThanOne = `<div markdown="0"><p class="total-results margin-y-0">${resultsTotal} ${type}s found</p></div>`;
+    const equalToOne = `<div markdown="0"><p class="total-results margin-y-0">${resultsTotal} ${type} found</p></div>`;
+    const lessThanOne = `<div markdown="0"><p class="total-results margin-y-0">No ${type}s found</p></div>`;
     if (type === null) {
         type = 'results'
     }
     if (resultsTotal > 1) {
         return moreThanOne;
-    } 
+    }
     if (resultsTotal === 1) {
         return equalToOne;
-    } 
+    }
     if (resultsTotal < 1) {
         return lessThanOne;
     }
