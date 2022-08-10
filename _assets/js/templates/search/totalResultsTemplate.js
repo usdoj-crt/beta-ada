926ad58f9f385509d907d6ce3d4189d5b3075526
JackRyan1989 <jackryan@navapbc.com>
2022-08-10 12:05:38 -0400

    Moving live region info to JS from HTML

--- _assets/js/templates/search/totalResultsTemplate.js
+++ _assets/js/templates/search/totalResultsTemplate.js
@@ -1,7 +1,7 @@
 export default function totalResults(resultsTotal=0, type="results") {
-    const moreThanOne = `<div markdown="0"><p class="total-results margin-y-0">${resultsTotal} ${type}s found</p></div>`;
-    const equalToOne = `<div markdown="0"><p class="total-results margin-y-0">${resultsTotal} ${type} found</p></div>`;
-    const lessThanOne = `<div markdown="0"><p class="total-results margin-y-0">No ${type}s found</p></div>`;
+    const moreThanOne = `<div markdown="0" role="status" aria-live="polite"><p class="total-results margin-y-0">${resultsTotal} ${type}s found</p></div>`;
+    const equalToOne = `<div markdown="0" role="status" aria-live="polite"><p class="total-results margin-y-0">${resultsTotal} ${type} found</p></div>`;
+    const lessThanOne = `<div markdown="0" role="status" aria-live="polite"><p class="total-results margin-y-0">No ${type}s found</p></div>`;
     if (type === null) {
         type = 'results'
     }
