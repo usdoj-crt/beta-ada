ef4b9157308eaf006138505adcb7bf1f3f322a1d
JackRyan1989 <jackryan@navapbc.com>
2022-09-02 13:30:22 -0400

    Initial creation of back to top links. Needs review.

--- _plugins/back_to_top.rb
+++ _plugins/back_to_top.rb
@@ -1,34 +1,15 @@
 require "nokogiri"
 
-class BackToTop < Jekyll::Generator
-  def generate(site)
-
-    @site = site
-    @parser = Jekyll::Converters::Markdown.new(@site.config)
-
-    # To Do:
-    # 1. Create variable that stores the HTML for the back to top button
-    # 2. Grab only the pages we want this on: Design Standards - DONE
-    # 3. Find each H2, H3, H4 and H5 element
-    # 4. Append the HTML variable to each heading defined above.
-
-    designstandards1991 = 'ADA Standards for Accessible Design Title III Regulation 28 CFR Part 36 (1991)'
-    designstandards2010 = '2010 ADA Standards for Accessible Design'
-
-    def self.addLinks(content)
-        html = '<p>Back to top</p>'
-        doc = Nokogiri::HTML(@parser.convert(content))
+Jekyll::Hooks.register :pages, :post_convert do |page|
+    if (page.name == '1991-design-standards.md' || page.name == '2010-stds.md')
+        html = '<a href="#top"><p>Back to top</p></a>'
+        doc = Nokogiri::HTML.parse(page.content)
         doc.css('h2, h3, h4').each do |heading|
-            heading.add_next_sibling '<p>Back to top</p>'
+            heading.add_child(html)
         end
-        return doc
+        page.content = doc.inner_html
     end
+end
 
-    site.pages.each do |page|
-        if page.ext == ".md" && (page.data['title'] == designstandards1991 || page.data['title'] == designstandards2010)
-            page.content = addLinks(page.content)
-        end
-    end
-end
-end
+
 
