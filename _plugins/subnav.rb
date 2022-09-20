74e879872277809245caa49d962cac267fa09ea7
JackRyan1989 <jackryan@navapbc.com>
2022-09-20 13:21:08 -0400

    Refactor of templates and subnav generator

--- _plugins/subnav.rb
+++ _plugins/subnav.rb
@@ -10,35 +10,60 @@ class Subnav < Jekyll::Generator
       if docs.length > 0
         docs.each do |doc|
           content = Nokogiri::HTML(parser.convert(doc.content))
-          doc.data["subnav"] = []
-          content.css('*').each do |heading|
-            if heading.name == 'h2'
-              doc.data["subnav"] << { "title" => heading.text, "url" => [doc.url, heading['id']].join("#") }
-            end
-            if heading.name == 'p' and heading.text.include? "expand-heading="
-              title = heading.text[heading.text=~/=/..-3].gsub("=", "").strip
-              title = title[1..-2]
-              doc.data["subnav"] << { "title" => title, "url" => [doc.url, title.downcase.gsub(" ", "-")].join("#") }
-            end
-          end
+          process_headings(doc, content)
         end
       end
     end
- 
+
     site.pages.each do |page|
       if page.ext == ".md"
         doc = Nokogiri::HTML(parser.convert(page['content']))
-        page.data["subnav"] = []
-        doc.css('*').each do |heading|
-          if heading.name == 'h2'
-            page.data["subnav"] << { "title" => heading.text, "url" => [page.url, heading['id']].join("#") }
-          end
-          if heading.name == 'p' and heading.text.include? "expand-heading="
-            title = heading.text[heading.text=~/=/..-3].gsub("=", "").strip
-            title = title[1..-2]
-            page.data["subnav"] << { "title" => title, "url" => [page.url, title.downcase.gsub(" ", "-")].join("#") }
+        process_headings(page, doc)
+        end
+      end
+  end
+
+  # Step through each heading and append to the page data
+  def process_headings(document, heading_list)
+      document.data["subnav"] = []
+      heading_list.css('*').each do |heading|
+        if heading.name == 'h2'
+          document.data['subnav'] << to_nav_item(document, heading).tap do |item|
+            item["children"] = subheadings(heading).map { |h3| to_nav_item(document, h3) }
           end
         end
+        if heading.name == 'p' and heading.text.include? "expand-heading="
+          title = heading.text[heading.text=~/=/..-3].gsub("=", "").strip
+          title = title[1..-2]
+          document.data['subnav'] << to_nav_item(document, title).tap do |item|
+            item["children"] = subheadings(heading).map { |h3| to_nav_item(document, h3) }
+          end
+        end
+      end
+  end
+
+  # Converts a heading into a hash of the info for a link
+  def to_nav_item(page, heading)
+    if heading.respond_to?(:text)
+      text = heading.text
+    else
+      text = heading
+    end
+    return {
+      "title" => text,
+      "url" => [page.url, heading['id']].join("#")
+    }
+  end
+
+  # Returns an enumerator of all H3s "belonging" to an H2
+  def subheadings(el)
+    Enumerator.new do |y|
+      next_el = el.next_sibling
+      while next_el && next_el.name != "h2"
+        if next_el.name == "h3"
+          y << next_el
+        end
+        next_el = next_el.next_sibling
       end
     end
   end
