665d1107cafb169bbd5f3a986335cd008489c44a
JackRyan1989 <jackryan@navapbc.com>
2022-10-26 19:46:00 -0400

    Updated javascript to selectively provide link

--- _assets/js/check-404.js
+++ _assets/js/check-404.js
@@ -1,14 +1,12 @@
-// Grab the current URL
-// Check if the pathname exists at the legacy site
-// Append the remainder of the URL onto constant archive.ada.gov
-// Put that link into a suggestion area in DOM
-
-const archiveURL = 'https://www.ada.gov';
-const targetDOMNode = document.getElementById('suggested-link');
+const archiveURL = 'https://archive.ada.gov';
+const targetDOMNode = document.getElementById('404-suggested-link');
 const currentPathName = document.location.pathname;
 let endpoint = `${archiveURL}${currentPathName}`;
 
-targetDOMNode.href = endpoint;
-targetDOMNode.innerText = endpoint;
+if ((currentPathName.includes('.doc')) || (currentPathName.includes('.docx')) || (currentPathName.includes('.php')) || (currentPathName.includes('.pdf'))) {
+    targetDOMNode.innerText = endpoint;
+    targetDOMNode.href = endpoint;
+}
+
 
 
