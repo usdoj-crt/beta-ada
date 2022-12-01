de2a8805590de7755fba719633cfa8e1db9ba8af
Jack Ryan <jackryan@navapbc.com>
2022-12-01 10:50:42 -0500

    Updating search params to remove beta search (#438)

--- _assets/js/utils/constants.js
+++ _assets/js/utils/constants.js
@@ -1,8 +1,8 @@
 // Pagination Constants:
 const NUMBER_OF_RESULTS = 20;
 const SEARCH_ENDPOINT = 'https://search.usa.gov/api/v2/search/i14y';
-const ACCESS_KEY = 'R9JA5YunOBaTGydUNm-oJjGCqKQ-zXsulsNskJVe5-c=';
-const AFFILIATE = 'justice-ada-beta';
+const ACCESS_KEY = 'Z8vERzCYuVMzAh2CKIRiyj1tRbOhdzseBGOuirGz1AQ=';
+const AFFILIATE = 'justice-ada';
 const TAGS = [
   'artificial-intelligence',
   'service-animals',
