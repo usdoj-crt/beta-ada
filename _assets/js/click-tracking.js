9e165db467a2ebb6f8d81e19681dc80943ae5938
JackRyan1989 <jackryan@navapbc.com>
2022-12-16 14:37:19 -0500

    Simplifying things by adding getSearchParams util function

--- _assets/js/click-tracking.js
+++ _assets/js/click-tracking.js
@@ -1,5 +1,6 @@
 // Import our constants from constandts.js
 import { CLICK_TRACKING_ENDPOINT, ACCESS_KEY, AFFILIATE, MODULE_CODE } from './utils/constants';
+import { getSearchParam } from './utils/searchParamUtils';
 
 export default function clickTracking() {
   // Get all the anchor tags that are children of b tags on the page:
@@ -8,16 +9,8 @@ export default function clickTracking() {
   // Loop through all the links and add a click event listener to each one:
   allLinks.forEach((link, index) => {
     const url = link.href;
-    const query = location.search.split('&').find((param) => param.startsWith('query='));
-    const position = location.search.split('&').includes('offset=') ?
-    Number(
-        location.search
-          .split('&')
-          .find((param) => param.startsWith('offset='))
-          .replace('offset=', '')
-      ) +
-      index +
-      1 : index + 1;
+    const query = encodeURI(getSearchParam('query'));
+    const position = Number(getSearchParam('offset')) + (index + 1);
     // What we need to send to the click tracking endpoint:
     // url clicked
     // search query
@@ -28,16 +21,13 @@ export default function clickTracking() {
     const endpoint = `${CLICK_TRACKING_ENDPOINT}?url=${url}&affiliate=${AFFILIATE}&access_key=${ACCESS_KEY}&module_code=${MODULE_CODE}&${query}&position=${position}`;
     // When a link is clicked, send a POST request to the search endpoint:
     link.addEventListener('click', () => {
-      fetch(
-        endpoint,
-        {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-            'Content-length': 0,
-          },
-        }
-      );
+      fetch(endpoint, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          'Content-length': 0,
+        },
+      });
     });
   });
 }
