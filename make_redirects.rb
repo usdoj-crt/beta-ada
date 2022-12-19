b6e5dca4ef7b2e12cb73b394c99f91121b2b8fdb
Kevin Kays <okkays@pm.me>
2022-12-18 22:30:09 -0500

    Make a script to generate redirects.
    - 🌎 The archive ada site has a bunch of static files (pdfs, etc) that
      we want to redirect to from the new site.
    - ⛔ There are a _lot_ of files, so doing that manually is tedious.
    - ✅ This commit adds a ruby script to generate a redirects.json with
      those pdfs.

---
+++ make_redirects.rb
@@ -0,0 +1,74 @@
+require 'optparse'
+require 'json'
+
+FILE_PATTERNS = [
+  '*.pdf',
+].freeze
+
+def parse_options
+  options = {}
+
+  OptionParser.new do |opts|
+    opts.banner = 'Usage: example.rb [options]'
+
+    opts.on('-sSRC', '--src=SRC', 'The directory to read from (default: ../ada)') do |src|
+      options[:src] = src
+    end
+
+    opts.on('-dDEST', '--dest=DEST', 'Set an output file (default: ./_data/redirects.json') do |dest|
+      options[:dest] = dest
+    end
+
+    opts.on('-pPREFIX', '--prefix=PREFIX', 'Set a prefix (default: https://archive.ada.gov)') do |prefix|
+      options[:prefix] = prefix
+    end
+
+    opts.on('-h', '--help', 'Prints this help') do
+      puts opts
+      exit
+    end
+  end.parse!
+
+  options[:dest] ||= './_data/redirects.json'
+  options[:src] ||= '../ada'
+  options[:prefix] ||= 'https://archive.ada.gov'
+
+  options
+end
+
+def get_files_to_redirect(options)
+  oldpwd = Dir.getwd
+  Dir.chdir(options[:src])
+  files = FILE_PATTERNS.flat_map do |pattern|
+    Dir.glob('**/' << pattern)
+  end
+  Dir.chdir(oldpwd)
+  files
+end
+
+def escape_files(files)
+  files.map do |file|
+    file.gsub(' ', '%20')
+  end
+end
+
+def make_map(files, options)
+  files.map do |file|
+    ['/' << file, File.join(options[:prefix], file)]
+  end.sort.to_h
+end
+
+def main
+  options = parse_options
+
+  files = escape_files(get_files_to_redirect(options))
+  redirects = make_map(files, options)
+  json_redirects = JSON.pretty_generate(redirects)
+  File.open(options[:dest], 'w') do |file|
+    file.write(json_redirects)
+  end
+
+  puts "Overwrote #{options[:dest]} with #{redirects.length} redirects to #{options[:prefix]} from #{options[:src]}"
+end
+
+main
