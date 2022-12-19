21c01217cdc9eb96d4ffc7d7c51b79c331afd818
Kevin Kays <okkays@pm.me>
2022-12-18 19:34:11 -0500

    Add a plugin to load simple redirects from json.
    - 🌎 We're currently loading redirects from markdown files
    - ⛔ There are a lot of redirects, and a lot of markdown files. Plus,
      the redirect plugin doesn't handle non-html redirects gracefully.
    - ✅ This commit adds a proof of concept for generating them from a json
      file, instead
    - 🔮 Future commits will add the full gambit of redirects

---
+++ _plugins/redirect_json.rb
@@ -0,0 +1,45 @@
+# Creates redirect pages given a redirects.json file.
+# Redirects.json should have the format:
+# {
+#   "/source-url": "https://destination-url/",
+#   ...
+# }
+
+module RedirectsJson
+  class RedirectsJsonPage < Jekyll::Page
+    # Use Jekyll's native absolute_url filter
+    include Jekyll::Filters::URLFilters
+
+    def initialize(site, from, to)
+      from += '.html' unless from.end_with?('.html')
+
+      @site = site
+      @name = from
+      process(@name)
+
+      @data ||= {
+        'sitemap' => false,
+        'permalink' => from,
+        'layout' => 'redirect',
+        'redirect' => {
+          'from' => from,
+          'to'   => to,
+        }
+      }
+    end
+
+    def context
+      JekyllRedirectFrom::Context.new(site)
+    end
+  end
+
+  class RedirectsJsonPageGenerator < Jekyll::Generator
+    def generate(site)
+      redirects = Jekyll::Collection.new(site, 'json_redirects')
+      site.collections['json_redirects'] = redirects
+      redirects.docs += site.data['redirects'].map do |from, to|
+        RedirectsJsonPage.new(site, from, to)
+      end
+    end
+  end
+end
