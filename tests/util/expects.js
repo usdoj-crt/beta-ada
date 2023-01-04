6705a9491ac4a475c0c1d326f7dd77c7cfe67943
Kevin Kays <Kevin.Kays@usdoj.gov>
2023-01-04 14:31:21 -0500

    Allow testing for dead redirects.
    - 🌎 We're adding regression testing for the bunch of manual markdown redirects that exist.
    - ⛔ A lot of the markdown redirects 404 when you actually follow them to the archive.
    - ✅ This commit exposes those failures by allowing tests to make real
      HEAD requests to external links.
    - 🔮 Future commits will hopefully fix the exposed broken redirects.

--- tests/util/expects.js
+++ tests/util/expects.js
@@ -1,14 +1,19 @@
 const { restrictToLocalhost } = require('./network');
 
+const CHECK_EXTERNAL_LINKS = process.env.CHECK_EXTERNAL_LINKS?.toLowerCase() === 'true';
+
 function normalizeRedirect(url) {
   return url
     .replace('index.html', '')
-    .replace(' ', '%20');
+    .replace(' ', '%20')
+    .replace(/\/$/, '');
 }
 
 export async function toRedirect(page, from, to) {
   restrictToLocalhost(page);
 
+  let success = false;
+  let status = 416;
   let errorText = "requestfailed wasn't called";
   let attemptedUrl;
   page.on('requestfailed', request => {
@@ -16,11 +21,21 @@ export async function toRedirect(page, from, to) {
     attemptedUrl = request.url();
   });
 
-  await page.goto(from);
+  page.on('response', response => {
+    if (normalizeRedirect(response.url()) !== to) return;
+    status = response.status();
+  });
+
+  page.on('load', page => {
+    success = true;
+    attemptedUrl = page.url();
+  });
+
+  const response = await page.goto(from);
 
   // Confirm this failed because we're blocking external urls to avoid making
   // 900 requests to prod, and not because of an unexpected reason.
-  if (!errorText.includes('net::ERR_BLOCKED_BY_CLIENT')) {
+  if (!success && !errorText.includes('net::ERR_BLOCKED_BY_CLIENT')) {
     return {
       message: () => `The redirect failed for an unexpected reason: ${errorText}`,
       pass: false,
@@ -32,6 +47,13 @@ export async function toRedirect(page, from, to) {
       message: () => `Expected ${from} to redirect to ${to}, but was ${attemptedUrl}`,
       pass: false,
     };
+  }
+
+  if (status !== 200) {
+    return {
+      message: () => `Status ${status} redirecting from ${from} to ${to}`,
+      pass: false,
+    }
   }
 
   return { pass: true };
--- tests/util/network.js
+++ tests/util/network.js
@@ -1,5 +1,7 @@
-const LOCALHOST = 'http://localhost:4000';
+const JEKYLL = 'http://localhost:4000';
 const PROD = 'https://www.ada.gov';
+
+const CHECK_EXTERNAL_LINKS = process.env.CHECK_EXTERNAL_LINKS?.toLowerCase() === 'true';
 
 /** Prevents tests from making http requests outside of the local machine.
  *
@@ -7,22 +9,26 @@ const PROD = 'https://www.ada.gov';
  * accidental networking or redirects can make unnecessary extra load on
  * production services.
  */
-export async function restrictToLocalhost(page) {
+export async function restrictToLocalhost(page, options) {
   await page.route('**/*', (route) => {
     const url = route.request().url();
+    if (url.startsWith('http://localhost:')) return route.continue();
     if (url.startsWith(PROD)) {
       // Force requests to prod to use localhost instead.
       return route.fulfill({
         status: 301,
         headers: {
-          Location: url.replace(PROD, LOCALHOST),
+          Location: url.replace(PROD, JEKYLL),
         },
       });
     }
+    // This is an external (non-local, non-prod) link, such as:
+    // - archive.ada.gov
+    // - justice.gov
+    // - etc
+    // Only make HEAD requests to external links, to avoid DDoSing.
+    if (CHECK_EXTERNAL_LINKS) return route.continue({method: 'HEAD'});
     // Block any other requests (e.g., to archive.ada.gov).
-    if (!url.startsWith(LOCALHOST)) {
-      return route.abort('blockedbyclient');
-    }
-    return route.continue();
+    return route.abort('blockedbyclient');
   });
 }
