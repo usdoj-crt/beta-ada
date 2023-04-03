850203ae52e9400f16c2ed8c85344999d492a901
Kevin Kays <Kevin.Kays@usdoj.gov>
2023-04-03 12:20:33 -0400

    Add a script to gather page commit dates.
    - 🌎 Search engine recency requires accurate commit dates.
    - ⛔ Right now, other results are prioritized over our dates because we
      don't have up-to-date numbers to give them.
    - ✅ This commit adds a script to gather commit dates.
    - 🔮 Future commits can expand on this to gather other automated info
      that we need for site content management.

---
+++ scripts/print_dates.sh
@@ -0,0 +1,14 @@
+outfile="$(git rev-parse --show-toplevel)/scripts/out/dates.csv"
+
+mkdir -p out
+echo "⌛ This might take a minute, please wait..."
+echo ""
+
+echo "Date|File" > "$outfile"
+for file in $(git ls-files '*.md'); do
+  date="$(git log -1 --no-decorate --pretty=format:%cs "$file" 2>&1)"
+  echo "$date|$file" | tee -a "$outfile"
+done
+
+echo ""
+echo "🎉 Dates written to $outfile"
