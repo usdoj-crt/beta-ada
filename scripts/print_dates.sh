e67fb2b8e2e8297529aa37aa0ddebca64e60e788
Kevin Kays <Kevin.Kays@usdoj.gov>
2023-04-03 12:25:14 -0400

    Only grab pages we care about

--- scripts/print_dates.sh
+++ scripts/print_dates.sh
@@ -6,6 +6,14 @@ echo ""
 
 echo "Date|File" > "$outfile"
 for file in $(git ls-files '*.md'); do
+  if [[ ! "$file" =~ ^_pages ]]; then
+    continue
+  fi
+
+  if [[ "$file" =~ ^_pages/redirects ]]; then
+    continue
+  fi
+
   date="$(git log -1 --no-decorate --pretty=format:%cs "$file" 2>&1)"
   echo "$date|$file" | tee -a "$outfile"
 done
