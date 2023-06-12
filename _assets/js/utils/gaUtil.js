803408112717c5c266d3e99a49baf4b0d33fed44
kewitham <katiewitham@navapbc.com>
2023-06-12 13:22:34 -0500

    add more click events

--- _assets/js/utils/gaUtil.js
+++ _assets/js/utils/gaUtil.js
@@ -1,5 +1,8 @@
-function sendGAClickEvent(event_name) {
-    gtag('event', 'click', { event_name: event_name });
+function sendGAClickEvent(e) {
+  e.preventDefault();
+  const event_name = e.target.ariaLabel + ' ' + e.target.innerText;
+  gtag('event', 'click', { event_name: event_name });
+  window.location.href = e.target.href;
 }
 
 function gtag() {
@@ -10,7 +13,26 @@ function gtag() {
 export default function initGAEvents() {
   const infoBoxLinks = document.getElementsByClassName("info-box-link");
   Array.from(infoBoxLinks).forEach((el) => {
-    el.addEventListener('click', sendGAClickEvent(el.innerText));
+    el.addEventListener('click', sendGAClickEvent);
   });
-  relatedReportButton.addEventListener('click', sendGAClickEvent('view related reports'));
+  const relatedContentLinks = document.getElementsByClassName("related-content-link");
+  Array.from(relatedContentLinks).forEach((el) => {
+    el.addEventListener('click', sendGAClickEvent);
+  });
+  const alertLinks = document.getElementsByClassName("alert-link");
+  Array.from(alertLinks).forEach((el) => {
+    el.addEventListener('click', sendGAClickEvent);
+  });
+  const topicCardLinks = document.getElementsByClassName("topic-card__link");
+  Array.from(topicCardLinks).forEach((el) => {
+    el.addEventListener('click', sendGAClickEvent);
+  });
+  const learnLinks = document.getElementsByClassName("learn-link");
+  Array.from(learnLinks).forEach((el) => {
+    el.addEventListener('click', sendGAClickEvent);
+  });
+  const sideNavLinks = document.getElementsByClassName("crt-sidenav-subnav-item");
+  Array.from(sideNavLinks).forEach((el) => {
+    el.addEventListener('click', sendGAClickEvent);
+  });
 }
