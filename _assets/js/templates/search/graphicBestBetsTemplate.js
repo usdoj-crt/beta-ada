ba940fbc3646a323ace2f862e3c97329db2fde63
kewitham <katiewitham@navapbc.com>
2023-07-07 14:53:24 -0500

    update styles

--- _assets/js/templates/search/graphicBestBetsTemplate.js
+++ _assets/js/templates/search/graphicBestBetsTemplate.js
@@ -1,12 +1,12 @@
 export default function graphicBestBetsTemplate(content) {
     const links = content.links.map(link => {
-        return `<a href="${link.url}" class="padding-right-2" aria-label="best bet box">${link.title}</a>`
+        return `<a href="${link.url}" class="padding-right-3 grid-col-6 padding-bottom-2" aria-label="best bet box">${link.title}</a>`
     });
     return `
-     <div class="padding-bottom-3 padding-top-2 usa-prose best-bet-item">
-       <p class="font-body-lg"><b class="title"><a href="${content.title_url}" class="best-bet" aria-label="best bet box">${content.title
+     <div class="padding-bottom-3 padding-top-2 usa-prose">
+       <p class="font-body-lg"><b class="title">${content.title
      .replace(/\uE000/g, '')
-     .replace(/\uE001/g, '')}</a></b></p>
+     .replace(/\uE001/g, '')}</b></p>
      <div class="grid-row flex-wrap flex-justify-start">
        ${links.join(' ')}
        </div>
