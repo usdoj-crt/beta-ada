b9d46f24c2ea16e6d5ad92bb1adc826550bff8a8
kewitham <katiewitham@navapbc.com>
2023-07-20 11:00:18 -0500

    add click tracking

--- _assets/js/templates/search/textBestBetsTemplate.js
+++ _assets/js/templates/search/textBestBetsTemplate.js
@@ -1,7 +1,7 @@
 export default function textBestBetsTemplate(content) {
    return `
     <div class="padding-bottom-3 padding-top-2 usa-prose best-bet-item">
-      <b class="title"><a href="${content.url}" class="best-bet" aria-label="best bet box">${content.title
+      <b class="title"><a href="${content.url}" class="best-bet" data-ga-event-name="best bet box">${content.title
     .replace(/\uE000/g, '')
     .replace(/\uE001/g, '')}</a></b>
       <div> ${content.description
