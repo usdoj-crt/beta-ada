d6fb3bee0305c80a541aec026668cb8e9f5fafe7
kewitham <katiewitham@navapbc.com>
2023-08-02 12:36:12 -0500

    subparts

--- _assets/js/laws-regs-parser.js
+++ _assets/js/laws-regs-parser.js
@@ -6,7 +6,7 @@ export default function parseLawsAndRegs () {
     if (!mainEl) return;
 
     const parser = new DOMParser();
-    const headingEls = ['h3', 'h4', 'h5', 'strong'];
+    const headingEls = ['h2', 'h3', 'h4', 'h5', 'strong'];
     const content = mainEl.innerHTML.toString();
     const parent = mainEl.parentElement;
     const newMainEl = document.createElement('div');
@@ -15,37 +15,57 @@ export default function parseLawsAndRegs () {
     const htmlDoc = parser.parseFromString(content, 'text/html');
     const elArr = Array.from(htmlDoc.children[0].children[1].childNodes);
     const sectionStrs = [];
-    let lastHeading = 0;
-
+    const subpartIndices = [];
+    const subparts = [];
+    let lastSectionHeading = 0;
     elArr.forEach((el, i) => {
         if (!(headingEls.includes(el.localName)
             || (el.localName === 'p' && el.firstChild.localName === 'strong')
             || i === elArr.length - 1)) return;
-        const sliceStr = elArr
-            .slice(lastHeading, i === elArr.length - 1 ? i : i - 1)
-            .map(element => element.outerHTML).join(' ');
-        sectionStrs.push(sliceStr);
-        lastHeading = i;
-    });
-
-    sectionStrs.forEach((section, i) => {
-        const div = document.createElement('div');
-        div.className = i === 0 ? 'intro-section' : 'section';
-        div.innerHTML = section.trim();
-        if (i !== 0 && Array.from(div.childNodes).length >= 2) {
-            const btnDiv = buildBtns();
-            div.firstChild.after(btnDiv);
-            const jumpLink = document.createElement('a');
-            jumpLink.id = i.toString();
-            div.prepend(jumpLink);
+        if (el.localName === 'h2') {
+            const subpart = document.createElement('div');
+            subpart.className = 'subpart';
+            subparts.push(subpart);
+            subpartIndices.push(sectionStrs.length + 1);
         }
-        newMainEl.appendChild(div);
+        const sectionStr = elArr
+            .slice(lastSectionHeading, i === elArr.length - 1 ? i : i - 1)
+            .map(el => el.outerHTML).join(' ');
+        sectionStrs.push(sectionStr);
+        lastSectionHeading = i;
     });
 
+    let currentSubpart = subparts[0];
+    sectionStrs.forEach((sectionStr, i) => {
+        const section = document.createElement('div');
+        section.className = i === 0 ? 'intro-section' : 'section';
+        section.innerHTML = sectionStr.trim();
+        if (subpartIndices.includes(i)) {
+          currentSubpart = subparts[subpartIndices.indexOf(i)];
+        } else if (i !== 0 && Array.from(section.childNodes).length >= 2) {
+            const btnDiv = buildBtns('.section');
+            section.firstChild.after(btnDiv);
+            const jumpLink = document.createElement('a');
+            jumpLink.id = 'section' + i.toString();
+            section.prepend(jumpLink);
+        }
+        currentSubpart.appendChild(section);
+    });
+
+    subparts.forEach((subpart, i) => {
+        if (Array.from(subpart.childNodes).length >= 2) {
+            const btnDiv = buildBtns('.subpart');
+            subpart.prepend(btnDiv);
+            const jumpLink = document.createElement('a');
+            jumpLink.id = 'subpart' + i.toString();
+            subpart.prepend(jumpLink);
+        }
+        newMainEl.appendChild(subpart);
+    });
     parent.appendChild(newMainEl);
 }
 
-function buildBtns() {
+function buildBtns(divType) {
     const btnDiv = document.createElement('div');
     btnDiv.className = 'btn-group display-flex flex-row flex-justify maxw-card';
     const btnTypes = ['Share', 'Copy', 'Print'];
@@ -57,22 +77,22 @@ function buildBtns() {
         btn.href = '#'
         if (btnType === 'Share') {
             btn.innerHTML = '<span class="copied-link text-no-underline" style="display:none;">Copied share link</span><svg class="usa-icon share-icon" aria-hidden="true" focusable="false" role="img"><use xlink:href="/assets/img/sprite.svg#share"></use></svg>';
-            btn.addEventListener('click', shareLink);
+            btn.addEventListener('click', (e) => { shareLink(e, divType) });
         } else if (btnType === 'Copy') {
             btn.innerHTML = '<span class="copied text-no-underline" style="display:none;">Copied text</span><svg class="usa-icon copy-icon" aria-hidden="true" focusable="false" role="img"><use xlink:href="/assets/img/sprite.svg#content_copy"></use></svg>';
-            btn.addEventListener('click', copyText);
+            btn.addEventListener('click', (e) => { copyText(e, divType) });
         } else if (btnType === 'Print') {
             btn.innerHTML = '<svg class="usa-icon" aria-hidden="true" focusable="false" role="img"><use xlink:href="/assets/img/sprite.svg#print"></use></svg>';
-            btn.addEventListener('click', printText);
+            btn.addEventListener('click', (e) => { printText(e, divType) });
         }
         btnDiv.appendChild(btn);
     });
     return btnDiv;
 }
 
-function copyText(e) {
+function copyText(e, divType) {
     e.preventDefault();
-    const section = e.target.closest('.section');
+    const section = e.target.closest(divType);
     const selection = window.getSelection();
     const range = document.createRange();
     range.selectNodeContents(section);
@@ -91,9 +111,9 @@ function copyText(e) {
     }, 2000);
 }
 
-function printText(e) {
+function printText(e, divType) {
     e.preventDefault();
-    const section = e.target.closest('.section');
+    const section = e.target.closest(divType);
     const winPrint = window.open('', '', 'left=0,top=0,width=800,height=900,toolbar=0,scrollbars=0,status=0');
     winPrint.document.write(section.outerHTML);
     winPrint.document.getElementsByClassName('btn-group')[0].style.display = 'none';
@@ -103,9 +123,9 @@ function printText(e) {
     winPrint.close();
 }
 
-function shareLink(e) {
+function shareLink(e, divType) {
     e.preventDefault();
-    const section = e.target.closest('.section');
+    const section = e.target.closest(divType);
     const url = window.location.href + '#' + section.firstChild.id;
     navigator.clipboard.writeText(url);
     const copyLinkText = section.getElementsByClassName('copied-link')[0];
