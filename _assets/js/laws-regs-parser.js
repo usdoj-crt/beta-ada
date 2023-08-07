829c070ec523f60e2ed701f577686ebe611cebd1
kewitham <katiewitham@navapbc.com>
2023-08-07 11:17:19 -0500

    add subpart parsing

--- _assets/js/laws-regs-parser.js
+++ _assets/js/laws-regs-parser.js
@@ -1,65 +1,66 @@
-export default function parseLawsAndRegs (mainEl) {
-    if (!mainEl) return;
+export default function parseLawsAndRegs (mainContent) {
+    if (!mainContent) return;
 
     const parser = new DOMParser();
-    const headingEls = ['h2', 'h3', 'h4', 'h5', 'strong'];
-    const content = mainEl.innerHTML.toString();
-    const parent = mainEl.parentElement;
-    const newMainEl = document.createElement('div');
-    newMainEl.className = 'interactive-headers';
-    mainEl.remove();
+    const headings = ['h2', 'h3', 'h4', 'h5', 'strong'];
+    const content = mainContent.innerHTML.toString();
+    const parent = mainContent.parentElement;
+    const newMainContent = document.createElement('div');
+    newMainContent.className = 'interactive-headers';
     const htmlDoc = parser.parseFromString(content, 'text/html');
     const contentNodes = Array.from(htmlDoc.children[0].children[1].childNodes);
-    const sectionStrs = [];
-    const subpartIndices = [];
-    const subparts = [];
-    let lastSectionHeading = 0;
-    contentNodes.forEach((node, i) => {
-        if (!(headingEls.includes(node.localName)
-            || (node.localName === 'p' && node.firstChild.localName === 'strong')
+    const sections = [];
+    const firstSubpart = document.createElement('div');
+    firstSubpart.className = 'subpart';
+    const subpartIndices = [0];
+    const subparts = [firstSubpart];
+    let lastHeading = 0;
+    contentNodes.map((node, i) => {
+        if (!(headings.includes(node.localName)
             || i === contentNodes.length - 1)) return;
         if (node.localName === 'h2') {
             const subpart = document.createElement('div');
             subpart.className = 'subpart';
             subparts.push(subpart);
-            subpartIndices.push(sectionStrs.length + 1);
+            subpartIndices.push(sections.length + 1);
         }
-        const sectionStr = contentNodes
-            .slice(lastSectionHeading, i)
+        const sectionText = contentNodes
+            .slice(lastHeading, i)
             .map(node => node.outerHTML).join(' ');
-        sectionStrs.push(sectionStr);
-        lastSectionHeading = i;
+        sections.push(sectionText);
+        lastHeading = i;
     });
 
     let currentSubpart = subparts[0];
-    sectionStrs.forEach((sectionStr, i) => {
-        if (!sectionStr.length) return;
-        const section = document.createElement('div');
-        section.className = 'section';
-        section.innerHTML = sectionStr.trim();
-        if (subpartIndices.includes(i)) {
-          currentSubpart = subparts[subpartIndices.indexOf(i)];
-        } else if (i !== 0 && Array.from(section.childNodes).length >= 2) {
+    sections.map((section, i) => {
+        if (!section.length) return;
+        const sectionContainer = document.createElement('div');
+        sectionContainer.className = 'section';
+        sectionContainer.innerHTML = section.trim();
+        currentSubpart = subpartIndices.includes(i) ? subparts[subpartIndices.indexOf(i)] : currentSubpart;
+        if (i !== 0 && Array.from(sectionContainer.childNodes).length >= 2 && !subpartIndices.includes(i)) {
             const btnDiv = buildBtns('.section');
-            section.firstChild.after(btnDiv);
+            sectionContainer.firstChild.after(btnDiv);
             const jumpLink = document.createElement('a');
             jumpLink.id = 'section' + i.toString();
-            section.prepend(jumpLink);
+            sectionContainer.prepend(jumpLink);
         }
-        currentSubpart.appendChild(section);
+        currentSubpart.appendChild(sectionContainer);
     });
 
-    subparts.forEach((subpart, i) => {
-        if (Array.from(subpart.childNodes).length >= 2) {
+    subparts.map((subpart, i) => {
+        if (Array.from(subpart.childNodes).length >= 1) {
             const btnDiv = buildBtns('.subpart');
-            subpart.getElementsByClassName('section')[0].firstChild.after(btnDiv);
+            console.log(subpart.childNodes)
+            subpart.getElementsByTagName('h2')[0]?.after(btnDiv);
             const jumpLink = document.createElement('a');
             jumpLink.id = 'subpart' + i.toString();
             subpart.prepend(jumpLink);
         }
-        newMainEl.appendChild(subpart);
+        newMainContent.appendChild(subpart);
     });
-    parent.appendChild(newMainEl);
+    parent.insertBefore(newMainContent, mainContent);
+    mainContent.remove()
 }
 
 function buildBtns(divType) {
@@ -71,7 +72,7 @@ function buildBtns(divType) {
         btn.className = btnType + '-btn text-no-underline section-btn';
         btn.setAttribute('data-ga-event-name', btnType);
         btn.setAttribute('aria-label', btnType);
-        btn.href = '#'
+        btn.href = '#';
         if (btnType === 'Share') {
             btn.innerHTML = '<span class="copied-link text-no-underline" style="display:none;">Copied share link</span><svg class="usa-icon share-icon" aria-hidden="true" focusable="false" role="img"><use xlink:href="/assets/img/sprite.svg#share"></use></svg>';
             btn.addEventListener('click', (e) => { shareLink(e, divType) });
@@ -113,7 +114,7 @@ function printText(e, divType) {
     const section = e.target.closest(divType);
     const winPrint = window.open('', '', 'left=0,top=0,width=800,height=900,toolbar=0,scrollbars=0,status=0');
     winPrint.document.write(section.outerHTML);
-    winPrint.document.getElementsByClassName('btn-group')[0].style.display = 'none';
+    Array.from(winPrint.document.getElementsByClassName('btn-group')).forEach(btnGroup => btnGroup.style.display = 'none');
     winPrint.document.close();
     winPrint.focus();
     winPrint.print();
