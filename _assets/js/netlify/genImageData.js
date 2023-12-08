418e20dcbc55024fd441e04fde31e437aa3037dc
kewitham <katiewitham@navapbc.com>
2023-12-08 14:43:38 -0600

    fix link and meta data issues

--- _assets/js/netlify/genImageData.js
+++ _assets/js/netlify/genImageData.js
@@ -1,51 +1,44 @@
-import { Octokit } from '@octokit/core';
+import { Octokit } from "@octokit/core";
 
 const octokit = new Octokit();
 const rawAssetURL = 'https://raw.githubusercontent.com/usdoj-crt/beta-ada/';
 const assetFileTypes = ['pdf', 'jpg', 'jpeg', 'png'];
 export default async function genImageData() {
-  const currentPage = window.location.href.split('/');
-  const collection = currentPage[6] != 'pages' ? currentPage[6]?.concat('/') : null;
-  const ref =
-    currentPage.includes('entries') && collection != null
-      ? 'cms/' + collection + currentPage[8]
-      : null;
+    const currentPage = window.location.href.split('/');
+    const collection = currentPage[6] != "pages"
+        ? currentPage[6]?.concat('/')
+        : null;
+    const ref = currentPage.includes('entries') && collection != null
+        ? 'cms/' + collection + currentPage[8]
+        : null;
 
-  const newImagePath = await getNewImagePath(ref);
+    const newImagePath = await getNewImagePath(ref);
 
-  const assetContentResponse = await octokit.request(
-    'GET /repos/usdoj-crt/beta-ada/contents/assets/',
-    {
-      owner: 'usdoj-crt',
-      repo: 'beta-ada',
-      path: 'assets/',
-      headers: {
-        'X-GitHub-Api-Version': '2022-11-28',
-      },
-    }
-  );
-  const imageFolderSha = assetContentResponse.data
-    .filter((folder) => folder['name'] === 'images')
-    .map((folder) => folder['sha'])[0];
-  const treeResponse = await octokit.request(
-    'GET /repos/usdoj-crt/beta-ada/git/trees/' + imageFolderSha + '?recursive=1',
-    {
-      owner: 'usdoj-crt',
-      repo: 'beta-ada',
-      tree_sha: imageFolderSha,
-      headers: {
-        'X-GitHub-Api-Version': '2022-11-28',
-      },
-    }
-  );
+    const assetContentResponse = await octokit.request('GET /repos/usdoj-crt/beta-ada/contents/_assets/', {
+        owner: 'usdoj-crt',
+        repo: 'beta-ada',
+        path: '_assets/',
+        headers: {
+        'X-GitHub-Api-Version': '2022-11-28'
+        }
+    });
+    const imageFolderSha = assetContentResponse.data.filter(folder => folder['name'] === 'images').map(folder => folder['sha'])[0];
+    const treeResponse = await octokit.request('GET /repos/usdoj-crt/beta-ada/git/trees/' + imageFolderSha + '?recursive=1', {
+        owner: 'usdoj-crt',
+        repo: 'beta-ada',
+        tree_sha: imageFolderSha,
+        headers: {
+            'X-GitHub-Api-Version': '2022-11-28'
+        }
+    });
 
-  const imageList = treeResponse.data.tree.filter((result) => result['type'] === 'blob');
+    const imageList = treeResponse.data.tree.filter(result => result['type'] === 'blob');
 
-  return { imageList, newImagePath };
+    return {imageList, newImagePath};
 }
 
 async function getNewImagePath(ref) {
-  if (!ref) return '';
+    if (!ref) return '';
 
     const commitSha = await octokit.request('GET /repos/usdoj-crt/beta-ada/commits/heads/' + ref, {
         owner: 'usdoj-crt',
@@ -68,7 +61,7 @@ async function getNewImagePath(ref) {
 
     const prNumber = PR.data[0]?.number;
 
-    if (!prNumber) return rawAssetURL + commitSha.data + '/assets/images/';
+    if (!prNumber) return rawAssetURL + commitSha.data + '/_assets/images/';
 
     const prFiles = await octokit.request('GET /repos/usdoj-crt/beta-ada/pulls/' + prNumber + '/files', {
         owner: 'usdoj-crt',
@@ -83,6 +76,6 @@ async function getNewImagePath(ref) {
         prFileArr.pop();
         return '/' + prFileArr.join('/') + '/';
     });
-    if (!fileNames.length) return rawAssetURL + commitSha.data + '/assets/images/';
+    if (!fileNames.length) return rawAssetURL + commitSha.data + '/_assets/images/';
     return rawAssetURL + commitSha.data + fileNames[0]
 }
--- assets/js/netlify/makeHTMLFromBodyContent.js
+++ assets/js/netlify/makeHTMLFromBodyContent.js
@@ -11,7 +11,8 @@ function makeHTMLFromBodyContent(bodyContent, variables, imageData) {
       .replaceAll("{{'", '')
       .replaceAll("' | relative_url}}", '')
       .replaceAll("'| relative_url}}", '')
-      .replaceAll("'|relative_url}}", '') || '';
+      .replaceAll("'|relative_url}}", '')
+    || '';
   const contentParts = content.split('{');
   contentParts.forEach((contentPart) => {
     if (contentPart.includes('% details') || contentPart.includes('% asset')) {
