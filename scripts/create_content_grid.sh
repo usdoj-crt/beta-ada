fbf77714b09f5a5e32d82275add0bbfdaf960963
kewitham <katiewitham@navapbc.com>
2024-01-17 11:09:15 -0600

    github action

--- scripts/create_content_grid.sh
+++ scripts/create_content_grid.sh
@@ -3,13 +3,14 @@ content_grid="$(git rev-parse --show-toplevel)/scripts/out/content_grid.xls"
 mkdir -p out
 
 echo "Title,Link,Publish Date,Updated Date,Description,Tags" > "$content_grid"
-top_level_files=../_pages/
-resource_files=../_pages/_resources/
-topic_files=../_pages/_topics/
-es_files=../_pages/es/
-law_regs_files=../_pages/law-and-regs/
-notice_files=../_pages/notices/_posts/
-for file in "$top_level_files"*.md "$resource_files"*.md "$topic_files"*.md "$es_files"*.md "$law_regs_files"*.md "$notice_files"*.md; do
+top_level_files=../../_pages/
+resource_files=../../_pages/_resources/
+topic_files=../../_pages/_topics/
+es_files=../../_pages/es/
+law_regs_files=../../_pages/law-and-regs/
+design_standards_files=../../_pages/law-and-regs/design-standards/
+notice_files=../../_pages/notices/_posts/
+for file in "$top_level_files"*.md "$resource_files"*.md "$topic_files"*.md "$es_files"*.md "$law_regs_files"*.md "$design_standards_files"*.md "$notice_files"*.md; do
   title="$(sed -n 's/^title:\(.*\)/\1/p' < $file | sed 's/,//g')"
   publish_date="$(sed -n 's/^publish-date:\(.*\)/\1/p' < $file)"
   updated_date="$(sed -n 's/^updated-date:\(.*\)/\1/p' < $file)"
