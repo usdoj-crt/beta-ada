79b24be2d6958e0541cb68b22f6dd78577774dec
kewitham <katiewitham@navapbc.com>
2024-01-19 14:39:43 -0600

    update styles for faceted search

--- assets/js/templates/search/searchResultsTemplate.js
+++ assets/js/templates/search/searchResultsTemplate.js
@@ -1,14 +1,21 @@
+function getDescription(content){
+  if (content.searchgov_custom1) {
+    const custom_description = content.searchgov_custom1[0].replace(",", ", ")
+    return custom_description.charAt(0).toUpperCase() + custom_description.slice(1)
+  } else {
+    return content.snippet.replace(/\uE000/g, '<span class="bg-yellow">').replace(/\uE001/g, '</span>')
+  }
+}
+
 export default function searchResultsTemplate(content) {
+  const description = getDescription(content);
   return `
         <li class="padding-bottom-5 margin-top-4 usa-prose border-bottom-05 border-base-lightest">
         <b class="title"><a class='search-result' href="${content.url}">${content.title
     .replace(/\uE000/g, '<span class="bg-yellow">')
     .replace(/\uE001/g, '</span>')}</a></b>
       <div class="text-base content-url"> ${content.url} </div>
-      <div> ${
-        content.searchgov_custom1 ??
-        content.snippet.replace(/\uE000/g, '<span class="bg-yellow">').replace(/\uE001/g, '</span>')
-      } </div>
+      <div> ${description} </div>
       </li>
 `;
 }
