80f51d22eb9eb302dfb0f532869e72dbe9596cbd
kewitham <katiewitham@navapbc.com>
2024-02-01 09:20:19 -0600

    add file path

--- scripts/create_content_grid.sh
+++ scripts/create_content_grid.sh
@@ -16,9 +16,7 @@ for file in "$top_level_files"*.md "$resource_files"*.md "$topic_files"*.md "$es
   updated_date="$(sed -n 's/^updated-date:\(.*\)/\1/p' < $file)"
   file_path="$(readlink -nf $file)"
   path="$(echo $file_path | cut -d '_' -f 2- | sed 's/.*/_&/')"
-  full_path="https://github.com/usdoj-crt/beta-ada/blob/main/"
-  full_path+=$path
-  deployment_date="$(git log --follow --format=%ad --date default $full_path | tail -1)"
+  deployment_date="$(git log --follow --format=%ad --date default $path | tail -1)"
   description="$(sed -n 's/^description:\(.*\)/\1/p' < $file | sed 's/,//g')"
   tags="$(awk '/---/{p=0}p;/tags:/{p=1}' $file | tr -d '\n')"
 
@@ -28,7 +26,7 @@ for file in "$top_level_files"*.md "$resource_files"*.md "$topic_files"*.md "$es
     link="$(sed -n 's/^permalink:\(.*\)/\1/p' < $file)"
   fi
 
-  echo "$title,$link,$full_path,$deployment_date,$publish_date,$updated_date,$description,$tags" | tee -a "$content_grid"
+  echo "$title,$link,$path,$deployment_date,$publish_date,$updated_date,$description,$tags" | tee -a "$content_grid"
 done
 
 echo ""
