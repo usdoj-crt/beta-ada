bda49d512566681cdff638b79032c1eac98fd08e
kewitham <katiewitham@navapbc.com>
2024-02-02 14:34:18 -0600

    address comment

--- scripts/create_content_grid.sh
+++ scripts/create_content_grid.sh
@@ -15,7 +15,7 @@ for file in "$top_level_files"*.md "$resource_files"*.md "$topic_files"*.md "$es
   publish_date="$(sed -n 's/^publish-date:\(.*\)/\1/p' < $file)"
   updated_date="$(sed -n 's/^updated-date:\(.*\)/\1/p' < $file)"
   file_path="$(readlink -nf $file)"
-  deployment_date="$(git log --first-parent main --follow --format=%ad --date=short $file_path | tail -1)"
+  deployment_date="$(git log --first-parent --follow --format=%ad --date=short $file_path | tail -1)"
   description="$(sed -n 's/^description:\(.*\)/\1/p' < $file | sed 's/,//g')"
   tags="$(awk '/---/{p=0}p;/tags:/{p=1}' $file | tr -d '\n')"
 
@@ -25,7 +25,7 @@ for file in "$top_level_files"*.md "$resource_files"*.md "$topic_files"*.md "$es
     link="$(sed -n 's/^permalink:\(.*\)/\1/p' < $file)"
   fi
 
-  echo "$title,$link,$deployment_date,$publish_date,$updated_date,$description,$tags" | tee -a "$content_grid"
+  # echo "$title,$link,$deployment_date,$publish_date,$updated_date,$description,$tags" | tee -a "$content_grid"
 done
 
 echo ""
